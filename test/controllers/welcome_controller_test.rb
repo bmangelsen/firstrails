4a675e98efb9d2e18bb3029e126a41f52cd2eaca
Ben Mangelsen <bmangelsen@gmail.com>
2016-11-14 15:28:14 -0500

    Added minor styling and lorem test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,14 +3,20 @@ require 'pry'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_url
+    get "/"
     assert_response :success
   end
 
-  test "should get name route" do
-    get "/welcome/Ben"
+  test "should get name" do
+    get "/Ben"
     assert_response :success
     assert_match(/Ben/, response.body)
+  end
+
+  test "should get ipsum" do
+    get "/lorem/cat"
+    assert_response :success
+    assert_match(/world domination/, response.body)
   end
 
 end
